a74a9c79f278283813d5faf61292bde7f1a8075b
dlbopeneye <dlb.openeye@gmail.com>
2019-04-15 16:11:55 -0700

    RGB slide down info

--- app/src/main/java/org/danbrown/naniiro/MainActivity.java
+++ app/src/main/java/org/danbrown/naniiro/MainActivity.java
@@ -1,5 +1,6 @@
 package org.danbrown.naniiro;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -8,12 +9,13 @@ import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.graphics.BitmapFactory;
-import android.graphics.Bitmap;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,8 +30,9 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 public class MainActivity extends AppCompatActivity {
     
     static final int REQUEST_TAKE_PHOTO = 1;
-
     String mCurrentPhotoPath;
+
+    TextView mRGBText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +40,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button mRetakeButton = findViewById(R.id.mRetakeButton);
-        final Button mReviewButton = findViewById(R.id.mReviewButton);
-
+        // final Button mReviewButton = findViewById(R.id.mReviewButton);
+        mRGBText = (TextView) findViewById(R.id.mRGBText);
+        mRGBText.setVisibility(View.GONE);
         mRetakeButton.setOnClickListener(new View.OnClickListener() {
            public void onClick(View v) {
                dispatchTakePictureIntent();
            }
         });
-        mReviewButton.setOnClickListener(new View.OnClickListener() {
+        /* mReviewButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 reviewImage();
             }
-        });
+        }); */
 
         dispatchTakePictureIntent();
+    }
+
+    public static void slideUp(Context ctx, View v) {
+        Animation a = AnimationUtils.loadAnimation(ctx, R.anim.slide_up);
+        if (a != null) {
+            a.reset();
+            if (v != null) {
+                v.clearAnimation();
+                v.startAnimation(a);
+            }
+        }
+    }
+
+    public static void slideDown(Context ctx, View v) {
+        Animation a = AnimationUtils.loadAnimation(ctx, R.anim.slide_down);
+        if (a != null) {
+            a.reset();
+            if (v != null) {
+                v.clearAnimation();
+                v.startAnimation(a);
+            }
+        }
     }
 
     public void dispatchTakePictureIntent() {
@@ -78,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-            TextView mRGBColor = findViewById(R.id.mRGBColor);
+            TextView mRGBColor = findViewById(R.id.mRGBText);
             ImageView mImageView = findViewById(R.id.mImageView);
 
             Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
@@ -97,9 +123,9 @@ public class MainActivity extends AppCompatActivity {
 
             // colorToHSV(mRGB, mHSV);
 
-            String mColor = "[" + r + ", " + g + ", " + b + "]";
+            String mRGBString = "[" + r + ", " + g + ", " + b + "]";
 
-            mRGBColor.setText(mColor);
+            mRGBColor.setText(mRGBString);
             mImageView.setImageBitmap(mBlock);
         }
     }
@@ -120,9 +146,19 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
-    public void reviewImage() {
+    public void reviewImage(View v) {
         Intent intent = new Intent(this, ImageViewActivity.class);
         intent.putExtra(EXTRA_MESSAGE, mCurrentPhotoPath);
         startActivity(intent);
+    }
+
+    public void toggleContents (View v) {
+        if (mRGBText.isShown()) {
+            slideUp(this, mRGBText);
+            mRGBText.setVisibility(View.GONE);
+        } else {
+            mRGBText.setVisibility(View.VISIBLE);
+            slideDown(this, mRGBText);
+        }
     }
 }
